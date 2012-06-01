5d02d176bc24efc2355da6d6c6f9345e60be8eec
Fly Man <fly.man.opensim@gmail.com>
2012-06-01 19:47:33 +0200

    Bumped version up for upcoming release + Changed textcolor of CautionFields

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -35,8 +35,8 @@
 
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 6;
-const S32 LL_VERSION_PATCH = 3;
-const S32 LL_VERSION_BUILD = 2;
+const S32 LL_VERSION_PATCH = 4;
+const S32 LL_VERSION_BUILD = 1;
 
 const char * const LL_CHANNEL = "Astra";
 
